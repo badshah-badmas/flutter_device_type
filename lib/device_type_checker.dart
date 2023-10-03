32d5d73b7d1ac0052bb7c22d7fc5d33862856792
badshah <badshahbadu62@gmail.com>
2023-10-03 19:11:02 +0530

    isMobile and isTablet function added

--- lib/device_type_checker.dart
+++ lib/device_type_checker.dart
@@ -9,12 +9,40 @@ class DeviceType {
     if (Platform.isAndroid) {
       DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
       AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+
       return androidInfo.systemFeatures
           .contains('android.software.leanback_only');
     }
     return false;
   }
 
-  static final isTablet = false;
-  static final isMobile = false;
+  static Future<bool> isTablet() async {
+    if (Platform.isAndroid) {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      return androidInfo.displayMetrics.widthPx > 600 &&
+          !androidInfo.systemFeatures
+              .contains('android.software.leanback_only');
+    } else if (Platform.isIOS) {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      IosDeviceInfo iosDeviceInfo = await deviceInfo.iosInfo;
+      return iosDeviceInfo.model.contains('iPad');
+    }
+    return false;
+  }
+
+  static Future<bool> isMobile() async {
+    if (Platform.isAndroid) {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      return androidInfo.displayMetrics.widthPx <= 600 &&
+          !androidInfo.systemFeatures
+              .contains('android.software.leanback_only');
+    } else if (Platform.isIOS) {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      IosDeviceInfo iosDeviceInfo = await deviceInfo.iosInfo;
+      return iosDeviceInfo.model.contains('ios');
+    }
+    return false;
+  }
 }
